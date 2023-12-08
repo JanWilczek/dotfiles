5bd9764351d1cb00ff842c5ec396e137af96ce85
Jan Wilczek <janekw@vp.pl>
2023-12-08 08:49:15 +0100

    Fixed Windows wezterm config.

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,15 +6,15 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-if wezterm.target_triple == 'x86-64-pc-windows-msvc' then
-config.font = wezterm.font_with_fallback { 'Fira Code', 'DengXian', }
-config.default_prog = { 'pwsh', '-NoLogo' }
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+	config.font = wezterm.font_with_fallback { 'CaskaydiaCove Nerd Font', 'Fira Code', 'DengXian', }
+	config.default_prog = { 'pwsh', '-NoLogo' }
 else
-config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font', 'DengXian', }
+	config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font', 'DengXian', }
 end
 
 config.color_scheme = 'Colors (base16)'
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.8
 
 return config
 
