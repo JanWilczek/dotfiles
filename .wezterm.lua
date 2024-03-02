be3d947a42822c99c55e6eee6f422b9a3e00b92c
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-03-02 10:47:58 +0100

    Adjusted vertical split

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,19 +3,30 @@ local wezterm = require 'wezterm'
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-	config.font = wezterm.font_with_fallback { 'CaskaydiaCove Nerd Font', 'Fira Code', 'DengXian', 'JetBrains Mono' }
-	config.default_prog = { 'pwsh', '-NoLogo' }
-	config.font_size = 11
+  config.font = wezterm.font_with_fallback { 'CaskaydiaCove Nerd Font', 'Fira Code', 'DengXian', 'JetBrains Mono' }
+  config.default_prog = { 'pwsh', '-NoLogo' }
+  config.font_size = 11
 else
-	config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font', 'DengXian', }
+  config.font = wezterm.font_with_fallback { 'FiraCode Nerd Font', 'DengXian', }
 end
 
 config.color_scheme = 'Colors (base16)'
 config.window_background_opacity = 1
+
+config.keys = {
+  {
+    key = '"',
+    mods = 'CTRL|SHIFT|ALT',
+    action = wezterm.action.SplitPane {
+      direction = 'Down',
+      size = { Percent = 10 },
+    },
+  }
+}
 
 return config
 
