20482980604448c019082591c4498d2412955c78
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-03-29 09:16:10 +0100

    Added more fallback fonts

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ if wezterm.config_builder then
 end
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.font = wezterm.font_with_fallback { 'CaskaydiaCove Nerd Font', 'Fira Code', 'DengXian', 'JetBrains Mono' }
+  config.font = wezterm.font_with_fallback { 'CaskaydiaCove Nerd Font', 'Fira Code', 'DengXian', 'JetBrains Mono', 'Noto Color Emoji', 'Symbols Nerd Font Mono', 'Segoe UI Emoji', 'FiraCode Nerd Font' }
   config.default_prog = { 'pwsh', '-NoLogo' }
   config.font_size = 11
 else
